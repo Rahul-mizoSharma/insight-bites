7ba6d0dde673922989f5af106c4648a355957dad
Rahul Sharma <152794984+Rahul-mizoSharma@users.noreply.github.com>
2024-12-25 23:29:50 +0530

    Add files via upload

---
+++ simple_interest.py
@@ -0,0 +1,12 @@
+principal = 200  
+rate = 5        
+time = 5        
+
+simple_interest = (principal * rate * time) / 100
+total_amount = principal + simple_interest
+
+print(f"Principal Amount: Rs. {principal}")
+print(f"Interest Rate: {rate}%")
+print(f"Time Period: {time} years")
+print(f"Simple Interest: Rs. {simple_interest}")
+print(f"Total Amount: Rs. {total_amount}") 
