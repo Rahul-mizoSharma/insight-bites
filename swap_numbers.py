e5184fe2feeaf643a6dcd5d379a02f9038208744
Rahul Sharma <152794984+Rahul-mizoSharma@users.noreply.github.com>
2024-12-26 23:48:28 +0530

    Add files via upload

---
+++ swap_numbers.py
@@ -0,0 +1,11 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+print(f"Before swapping: num1 = {num1}, num2 = {num2}")
+
+
+temp = num1
+num1 = num2
+num2 = temp
+
+print(f"After swapping: num1 = {num1}, num2 = {num2}") 
