a0e029b90e9f921c0f3a93fac3363a29a295c04d
Rahul Sharma <152794984+Rahul-mizoSharma@users.noreply.github.com>
2024-12-30 23:50:50 +0530

    Add files via upload

---
+++ data.py
@@ -0,0 +1,12 @@
+d1 = {'Name': 'John Doe','Age': 30,'Profession':'Engineer'}
+
+print(d1['Profession']) #print the value of profession key
+
+d1['Age'] = 31
+d1['Country'] = 'USA'
+
+print(d1)
+
+del d1['Profession']
+
+print(d1)
